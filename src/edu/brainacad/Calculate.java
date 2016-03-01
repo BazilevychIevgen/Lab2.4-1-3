f4fb8d5ff92011224ec083bf28b7b1728649e58c
Admin <baro@ikrt>
2016-03-01 12:38:24 +0200

    Initial commit

---
+++ src/edu/brainacad/Calculate.java
@@ -0,0 +1,19 @@
+package edu.brainacad;
+
+/**
+ * Created by Admin on 01.03.2016.
+ */
+public class Calculate {
+    public static void main(String[] args) {
+
+        int[]arr1 = {5,7,15,8,6};
+        int[]arr2 = {10,4,8,7,6};
+
+        MyMath.findMax(arr1);
+        MyMath.findMin(arr2);
+
+        MyMath.areaOfCircle(20);
+
+
+    }
+}
---
+++ src/edu/brainacad/MyMath.java
@@ -0,0 +1,34 @@
+package edu.brainacad;
+
+import java.util.Arrays;
+
+/**
+ * Created by Admin on 01.03.2016.
+ */
+public class MyMath {
+
+    private static final double PI = 3.14;
+
+    public static void areaOfCircle(int radius){
+
+        double S = PI *(radius*radius);
+        System.out.println(S);
+    }
+
+    public static void findMax(int...value) {
+        Arrays.sort(value);
+        int Max = 0;
+
+        for (int element:value) {
+
+            Max = element;
+        }
+        System.out.println(Max);
+    }
+    public static void findMin(int...value){
+        Arrays.sort(value);
+        int Min = value[0];
+        System.out.println(Min);
+
+    }
+}
